c1ece0af9545d47170f17adca1107d1f44c06955
Yosef <yosef.saaid02@eng-st.cu.edu.eg>
2023-11-28 19:38:28 +0200

    edit code structure

---
+++ CMakeFiles/GAME_APPLICATION.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/yosefsaaid/dev/Graphics/Graphics-project/CMPN205 Project - Student Version/vendor/glad/src/gl.c" "CMakeFiles/GAME_APPLICATION.dir/vendor/glad/src/gl.c.o" "gcc" "CMakeFiles/GAME_APPLICATION.dir/vendor/glad/src/gl.c.o.d"
+  "/Users/yosefsaaid/dev/Graphics/Graphics-project/CMPN205 Project - Student Version/source/common/application.cpp" "CMakeFiles/GAME_APPLICATION.dir/source/common/application.cpp.o" "gcc" "CMakeFiles/GAME_APPLICATION.dir/source/common/application.cpp.o.d"
+  "/Users/yosefsaaid/dev/Graphics/Graphics-project/CMPN205 Project - Student Version/source/common/asset-loader.cpp" "CMakeFiles/GAME_APPLICATION.dir/source/common/asset-loader.cpp.o" "gcc" "CMakeFiles/GAME_APPLICATION.dir/source/common/asset-loader.cpp.o.d"
+  "/Users/yosefsaaid/dev/Graphics/Graphics-project/CMPN205 Project - Student Version/source/common/components/camera.cpp" "CMakeFiles/GAME_APPLICATION.dir/source/common/components/camera.cpp.o" "gcc" "CMakeFiles/GAME_APPLICATION.dir/source/common/components/camera.cpp.o.d"
+  "/Users/yosefsaaid/dev/Graphics/Graphics-project/CMPN205 Project - Student Version/source/common/components/free-camera-controller.cpp" "CMakeFiles/GAME_APPLICATION.dir/source/common/components/free-camera-controller.cpp.o" "gcc" "CMakeFiles/GAME_APPLICATION.dir/source/common/components/free-camera-controller.cpp.o.d"
+  "/Users/yosefsaaid/dev/Graphics/Graphics-project/CMPN205 Project - Student Version/source/common/components/mesh-renderer.cpp" "CMakeFiles/GAME_APPLICATION.dir/source/common/components/mesh-renderer.cpp.o" "gcc" "CMakeFiles/GAME_APPLICATION.dir/source/common/components/mesh-renderer.cpp.o.d"
+  "/Users/yosefsaaid/dev/Graphics/Graphics-project/CMPN205 Project - Student Version/source/common/components/movement.cpp" "CMakeFiles/GAME_APPLICATION.dir/source/common/components/movement.cpp.o" "gcc" "CMakeFiles/GAME_APPLICATION.dir/source/common/components/movement.cpp.o.d"
+  "/Users/yosefsaaid/dev/Graphics/Graphics-project/CMPN205 Project - Student Version/source/common/ecs/entity.cpp" "CMakeFiles/GAME_APPLICATION.dir/source/common/ecs/entity.cpp.o" "gcc" "CMakeFiles/GAME_APPLICATION.dir/source/common/ecs/entity.cpp.o.d"
+  "/Users/yosefsaaid/dev/Graphics/Graphics-project/CMPN205 Project - Student Version/source/common/ecs/transform.cpp" "CMakeFiles/GAME_APPLICATION.dir/source/common/ecs/transform.cpp.o" "gcc" "CMakeFiles/GAME_APPLICATION.dir/source/common/ecs/transform.cpp.o.d"
+  "/Users/yosefsaaid/dev/Graphics/Graphics-project/CMPN205 Project - Student Version/source/common/ecs/world.cpp" "CMakeFiles/GAME_APPLICATION.dir/source/common/ecs/world.cpp.o" "gcc" "CMakeFiles/GAME_APPLICATION.dir/source/common/ecs/world.cpp.o.d"
+  "/Users/yosefsaaid/dev/Graphics/Graphics-project/CMPN205 Project - Student Version/source/common/material/material.cpp" "CMakeFiles/GAME_APPLICATION.dir/source/common/material/material.cpp.o" "gcc" "CMakeFiles/GAME_APPLICATION.dir/source/common/material/material.cpp.o.d"
+  "/Users/yosefsaaid/dev/Graphics/Graphics-project/CMPN205 Project - Student Version/source/common/material/pipeline-state.cpp" "CMakeFiles/GAME_APPLICATION.dir/source/common/material/pipeline-state.cpp.o" "gcc" "CMakeFiles/GAME_APPLICATION.dir/source/common/material/pipeline-state.cpp.o.d"
+  "/Users/yosefsaaid/dev/Graphics/Graphics-project/CMPN205 Project - Student Version/source/common/mesh/mesh-utils.cpp" "CMakeFiles/GAME_APPLICATION.dir/source/common/mesh/mesh-utils.cpp.o" "gcc" "CMakeFiles/GAME_APPLICATION.dir/source/common/mesh/mesh-utils.cpp.o.d"
+  "/Users/yosefsaaid/dev/Graphics/Graphics-project/CMPN205 Project - Student Version/source/common/shader/shader.cpp" "CMakeFiles/GAME_APPLICATION.dir/source/common/shader/shader.cpp.o" "gcc" "CMakeFiles/GAME_APPLICATION.dir/source/common/shader/shader.cpp.o.d"
+  "/Users/yosefsaaid/dev/Graphics/Graphics-project/CMPN205 Project - Student Version/source/common/systems/forward-renderer.cpp" "CMakeFiles/GAME_APPLICATION.dir/source/common/systems/forward-renderer.cpp.o" "gcc" "CMakeFiles/GAME_APPLICATION.dir/source/common/systems/forward-renderer.cpp.o.d"
+  "/Users/yosefsaaid/dev/Graphics/Graphics-project/CMPN205 Project - Student Version/source/common/texture/sampler.cpp" "CMakeFiles/GAME_APPLICATION.dir/source/common/texture/sampler.cpp.o" "gcc" "CMakeFiles/GAME_APPLICATION.dir/source/common/texture/sampler.cpp.o.d"
+  "/Users/yosefsaaid/dev/Graphics/Graphics-project/CMPN205 Project - Student Version/source/common/texture/screenshot.cpp" "CMakeFiles/GAME_APPLICATION.dir/source/common/texture/screenshot.cpp.o" "gcc" "CMakeFiles/GAME_APPLICATION.dir/source/common/texture/screenshot.cpp.o.d"
+  "/Users/yosefsaaid/dev/Graphics/Graphics-project/CMPN205 Project - Student Version/source/common/texture/texture-utils.cpp" "CMakeFiles/GAME_APPLICATION.dir/source/common/texture/texture-utils.cpp.o" "gcc" "CMakeFiles/GAME_APPLICATION.dir/source/common/texture/texture-utils.cpp.o.d"
+  "/Users/yosefsaaid/dev/Graphics/Graphics-project/CMPN205 Project - Student Version/source/main.cpp" "CMakeFiles/GAME_APPLICATION.dir/source/main.cpp.o" "gcc" "CMakeFiles/GAME_APPLICATION.dir/source/main.cpp.o.d"
+  "/Users/yosefsaaid/dev/Graphics/Graphics-project/CMPN205 Project - Student Version/vendor/imgui/imgui.cpp" "CMakeFiles/GAME_APPLICATION.dir/vendor/imgui/imgui.cpp.o" "gcc" "CMakeFiles/GAME_APPLICATION.dir/vendor/imgui/imgui.cpp.o.d"
+  "/Users/yosefsaaid/dev/Graphics/Graphics-project/CMPN205 Project - Student Version/vendor/imgui/imgui_demo.cpp" "CMakeFiles/GAME_APPLICATION.dir/vendor/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/GAME_APPLICATION.dir/vendor/imgui/imgui_demo.cpp.o.d"
+  "/Users/yosefsaaid/dev/Graphics/Graphics-project/CMPN205 Project - Student Version/vendor/imgui/imgui_draw.cpp" "CMakeFiles/GAME_APPLICATION.dir/vendor/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/GAME_APPLICATION.dir/vendor/imgui/imgui_draw.cpp.o.d"
+  "/Users/yosefsaaid/dev/Graphics/Graphics-project/CMPN205 Project - Student Version/vendor/imgui/imgui_impl/imgui_impl_glfw.cpp" "CMakeFiles/GAME_APPLICATION.dir/vendor/imgui/imgui_impl/imgui_impl_glfw.cpp.o" "gcc" "CMakeFiles/GAME_APPLICATION.dir/vendor/imgui/imgui_impl/imgui_impl_glfw.cpp.o.d"
+  "/Users/yosefsaaid/dev/Graphics/Graphics-project/CMPN205 Project - Student Version/vendor/imgui/imgui_impl/imgui_impl_opengl3.cpp" "CMakeFiles/GAME_APPLICATION.dir/vendor/imgui/imgui_impl/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/GAME_APPLICATION.dir/vendor/imgui/imgui_impl/imgui_impl_opengl3.cpp.o.d"
+  "/Users/yosefsaaid/dev/Graphics/Graphics-project/CMPN205 Project - Student Version/vendor/imgui/imgui_widgets.cpp" "CMakeFiles/GAME_APPLICATION.dir/vendor/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/GAME_APPLICATION.dir/vendor/imgui/imgui_widgets.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
